f31661cc8c8a5eb87bfc2b96b2c9f63860c48cce
Sasha Petushkova <sashpet_2000@mail.ru>
2023-02-02 16:35:34 +0300

    complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+    n = String(n);
+
+    if (n[0] === '-') {
+        return n.split('').slice(1).reverse().join('');
+    }
+   return n.split('').reverse().join('');
 }
